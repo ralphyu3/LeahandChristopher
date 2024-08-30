aace3cc6d0e906b4914464a06a07a11b70bd93b4
ralphyu3 <ralphyu>
2024-08-30 16:43:25 +0800

    improve styke and music volume

--- scripts/playmusic.js
+++ scripts/playmusic.js
@@ -1,6 +1,7 @@
 document.getElementById('envelopeButton').addEventListener('click', function () {
     // Play the audio
     var audio = document.getElementById('audio');
+    audio.volume = 0.5;
     audio.play();
 
 });
