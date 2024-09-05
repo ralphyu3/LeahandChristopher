a58aedb6ed79f8125d9f68a51dc309717c64eefc
ralphyu3 <ralphyu>
2024-09-05 20:55:00 +0800

    update music

--- scripts/playmusic.js
+++ scripts/playmusic.js
@@ -1,7 +1,6 @@
 document.getElementById('envelopeButton').addEventListener('click', function () {
     // Play the audio
     var audio = document.getElementById('audio');
-    audio.volume = 0.5;
     audio.play();
 
 });
